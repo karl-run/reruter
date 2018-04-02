218a50620bc92479e1ccf3d077b0c7adc914e696
Karl J. Overå <karloveraa@gmail.com>
2018-04-02 21:17:25 +0200

    api/web: Enable travis

--- web/src/frame/Frame.js
+++ web/src/frame/Frame.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import styled from 'styled-components';
 
 const StyledFrame = styled.div`
